c8aa778c7a2aaf62d2802538767998be380ab7ed
Nikita <main.acr0matic@gmail.com>
2021-11-12 16:02:05 +0300

    feat: верстка "скидки"

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,8 +1,18 @@
-const swiper = new Swiper('.slider-categories', {
+const categories = new Swiper('.slider-categories', {
   loop: true,
   slidesPerView: 8,
   navigation: {
     nextEl: '.slider-categories__next',
     prevEl: '.slider-categories__prev',
+  },
+});
+
+const promotion = new Swiper('.slider-promotion', {
+  slidesPerView: 2,
+  spaceBetween: 35,
+
+  navigation: {
+    nextEl: '.slider-promotion__next',
+    prevEl: '.slider-promotion__prev',
   },
 });
